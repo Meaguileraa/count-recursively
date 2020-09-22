cb008c9739c82454a92be69ed46002184989691e
Marissa Aguilera <marissaguilera@yahoo.com>
2020-09-22 05:03:18 +0000

    Final commit

--- countrecursively.py
+++ countrecursively.py
@@ -14,6 +14,10 @@ For example:
 def count_recursively(lst):
     """Return number of items in a list, using recursion."""
 
+    if not lst: 
+        return 0 
+    return 1 + count_recursively(lst[1:])
+
 
 if __name__ == '__main__':
     import doctest
